f2606a8387ec586e2595d7c523a7c21acadb005b
Andrew Sokolov <asokolov@atlassian.com>
2021-02-13 16:52:57 +0200

    PROVIDER_SERVICE_KEY

--- scripts/setExternalAuthCreds.sh
+++ scripts/setExternalAuthCreds.sh
@@ -4,4 +4,4 @@ set -eux
 curl --location --request POST $FORGE_GRAPHQL_GATEWAY \
 -u $FORGE_EMAIL:$FORGE_API_TOKEN \
 --header 'Content-Type: application/json' \
---data-raw '{"query":"mutation setExternalAuthCredentials($input: SetExternalAuthCredentialsInput!) {\n  setExternalAuthCredentials(input: $input) {\n    success\n    statusCode\n    message\n  }\n}","variables":{"input":{"environment":{"appId":"ari:cloud:ecosystem::app/'"$FORGE_APP_ID"'","key":"'"$FORGE_ENV"'"},"serviceKey":"zendesk","credentials":{"clientId":"'"$PROVIDER_CLIENT_ID"'","clientSecret":"'"$PROVIDER_CLIENT_SECRET"'"}}}}'
+--data-raw '{"query":"mutation setExternalAuthCredentials($input: SetExternalAuthCredentialsInput!) {\n  setExternalAuthCredentials(input: $input) {\n    success\n    statusCode\n    message\n  }\n}","variables":{"input":{"environment":{"appId":"ari:cloud:ecosystem::app/'"$FORGE_APP_ID"'","key":"'"$FORGE_ENV"'"},"serviceKey":"'"$PROVIDER_SERVICE_KEY"'","credentials":{"clientId":"'"$PROVIDER_CLIENT_ID"'","clientSecret":"'"$PROVIDER_CLIENT_SECRET"'"}}}}'
